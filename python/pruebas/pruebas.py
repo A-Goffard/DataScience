133414e9a4918ffb7bdba365e291eef7e2544368
A-Goffard <aintzagoffard@gmail.com>
2025-03-17 12:51:38 +0100

    calculos

--- python/pruebas/pruebas.py
+++ python/pruebas/pruebas.py
@@ -8,12 +8,31 @@ def pedir_numero ():
     num = int(input("Inserta un número, por favor "))
     return num
 
+def menu():
+    print("Opciones: ")
+    print("1) Sumar: ")
+    print("2) Restar: ")
+    print("3) Salir: ")
+    opc = int(input("Elige una opción: "))
+    return opc
+
+def operativa(num1, num2, opc):
+    if opc == 1:
+        resul = sumar(num1, num2)
+    elif opc == 2:
+        resul = restar(num1, num2)
+    else:
+        print("Agur!")
+        print("True: ", True, "True int: ", int(True))
+        resul = True
+    return int(resul)
+
 def main():
+    opc = menu()
     num1 = pedir_numero()
     num2 = pedir_numero()
     
-    print(sumar(num1, num2))
-    print(restar(num1, num2))
+    operativa(num1, num2, opc)
     
     
 if __name__ == "__main__": 
